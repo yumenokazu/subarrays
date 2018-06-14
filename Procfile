4f4958c190f120a95a686e9809c81466bdf2f167
yumenokazu <yumenokazu@users.noreply.github.com>
2018-06-14 21:22:28 +0300

    separate gunicorn start

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:33507 app:create_app
+web: gunicorn --bind 0.0.0.0:$PORT wsgi
--- app.py
+++ app.py
@@ -1,40 +1,15 @@
-from flask import Flask, render_template, request, jsonify
-from os import environ
-
-app = Flask(__name__)
-from subarrays import subarrays, parse_string
-
-
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template("index.html")
-
-
-@app.route('/', methods=['POST'])
-@app.route('/index.html', methods=['POST'])
-def get_subarrays():
-
-    numbers = request.form['numbers']
-    target = request.form['target']
-    try:
-        numbers=parse_string(numbers)
-        target=int(target)
-        result = subarrays(numbers, target)
-        return jsonify({'status': 'success',
-                        'data': render_template("list_items.html", object_list=result)})
-    except ValueError as e:
-        return jsonify({'status': 'fail', 'data': e})
+from flask import Flask
 
 
 def create_app():
+    app = Flask(__name__)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    if environ.get('ENV') == 'HEROKU':
-        app.run(debug=False)
-    else:
-        app.run()
+    return app
 
+
+app = create_app()
+from views import *
 
 if __name__ == '__main__':
-    create_app()
+    app.run()
 
